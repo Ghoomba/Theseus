3c76a424d58becbcb6447ab632ba112688b4134e
StormASC20 <68335970+StormASC20@users.noreply.github.com>
2024-09-25 15:22:30 -0400

    please push I beg

---
+++ Siren Game/Assets/PlayerMovement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        moveSpeed = 3.0f;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Up/Down Movement
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.up * -moveSpeed * Time.deltaTime;
+        }
+
+        // Left/Right Movement
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.right * -moveSpeed * Time.deltaTime;
+
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+        }
+
+    }
+
+
+}
