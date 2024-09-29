1f1ee154b1f91aa5165428d0795abe1032a311f9
Hare Dunlap (RIT Student) <ird7384@rit.edu>
2024-09-29 16:17:41 -0400

    some frameworking stuff
    right now as a debug thing pressing space queues notes

--- Siren Game/Assets/Beats.cs
+++ Siren Game/Assets/Beats.cs
@@ -5,21 +5,51 @@ using UnityEngine;
 
 public class Beats : MonoBehaviour
 {
+    List<(float, int, GameObject)> timings = null;
+    float offset = 0f;
+    public GameObject noteBasis;
+    public GameObject staffObject;
 
+    const float noteStart = 0.5f;
+    const float staffPos = -0.3f;
     // Start is called before the first frame update
     void Start()
     {
-
+        timings = new List<(float, int, GameObject)> ();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        for (int i = 0; i < timings.Count; i++)
+        {
+            timings[i] = (timings[i].Item1 - Time.deltaTime, timings[i].Item2, timings[i].Item3);
+            if (timings[i].Item1 < 1.0f)
+            {
+                timings[i].Item3.SetActive(true);
+                timings[i].Item3.transform.localPosition = new Vector3(staffPos + timings[i].Item1 * (noteStart - staffPos), 0, 0);
+            }
+        }
+        for (int i = timings.Count-1; i >= 0; i--)
+        {
+            if (timings[i].Item1 < -1.0f)
+            {
+                Object.Destroy(timings[i].Item3);
+                timings.RemoveAt(i);
+            }
+        }
+        if (Input.GetKey(KeyCode.Space)) //DEBUG FUNCTIONALITY: PRESSING SPACE CUES A NOTE IN 3 BEATS AT 90 BPM
+        {
+            queueBeat(3, 90, 0);
+        }
     }
 
-    void queueBeat(double beatCount, double bpm)
-    {
-
+    void queueBeat(float beatCount, float bpm, int noteType)
+    { //beats per minute times minutes = beats; beatCount/bpm * 60
+        timings.Add((
+            (beatCount / bpm * 60) - offset,
+            noteType,
+            Object.Instantiate(noteBasis, staffObject.transform)
+        ));
     }
 }
--- Siren Game/Assets/Hunger.cs
+++ Siren Game/Assets/Hunger.cs
@@ -5,20 +5,35 @@ using UnityEngine;
 
 public class Hunger : MonoBehaviour
 {
-    public double hungerMax = 60.0f;
-    double hunger;
-    public double hungerDrainPerSecond = 1.0f;
-    double hungerDrainPerFrame;
+    public float hungerMax = 60.0f;
+    float hunger;
+    public float hungerDrainPerSecond = 1.0f;
+    public GameObject meter;
     // Start is called before the first frame update
     void Start()
     {
-        hungerDrainPerFrame = hungerDrainPerSecond / 60.0f;
         hunger = hungerMax;
     }
 
     // Update is called once per frame
     void Update()
     {
-        hunger -= hungerDrainPerFrame;
+        hunger -= hungerDrainPerSecond * Time.deltaTime;
+
+        float hungerPercentage = hunger / hungerMax;
+        meter.transform.localScale = new Vector3(hungerPercentage, 1, 1);
+        meter.transform.localPosition = new Vector3((hungerPercentage - 1) / 2, 0, -1);
+
+        if (hunger <= 0)
+        {
+            //todo: implement failure
+            hunger = 0;
+        }
+    }
+
+    void RestoreHunger(float food)
+    {
+        hunger += food;
+        hunger = Mathf.Min(hunger, hungerMax);
     }
 }
