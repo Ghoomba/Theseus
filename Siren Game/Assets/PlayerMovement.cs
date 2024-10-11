74470305a87e2e41111ccbfb5ce8f60e9a9dfa2e
ZackNAttack <ird7384@g.rit.edu>
2024-10-11 10:46:50 -0400

    obstacle collision

--- Siren Game/Assets/PlayerMovement.cs
+++ Siren Game/Assets/PlayerMovement.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -10,19 +11,21 @@ public class PlayerMovement : MonoBehaviour
     public float minY;
     public float maxX;
     public float maxY;
-
+    GameObject[] obstacles;
     // Start is called before the first frame update
     void Start()
     {
 
         moveSpeed = 6.0f;
 
-
+        obstacles = GameObject.FindGameObjectsWithTag("Obstacle");
     }
 
     // Update is called once per frame
     void Update()
     {
+        float oldX = transform.position.x;
+        float oldY = transform.position.y;
         // Up/Down Movement
         if (Input.GetKey(KeyCode.W))
         {
@@ -50,8 +53,47 @@ public class PlayerMovement : MonoBehaviour
         transform.position = new Vector3(
             Mathf.Max(minX, Mathf.Min(transform.position.x, maxX)),
             Mathf.Max(minY, Mathf.Min(transform.position.y, maxY)),
-            0
+        0
         );
+
+        foreach (GameObject obstacle in  obstacles)
+        {
+            BoxCollider2D collider = gameObject.GetComponent<BoxCollider2D>();
+            BoxCollider2D obstacleCollider = obstacle.GetComponent<BoxCollider2D>();
+            if (collider.bounds.Intersects(obstacleCollider.bounds))
+            {
+                //process collision
+                Vector3 closestPoint = obstacleCollider.ClosestPoint(transform.position);
+                if (Mathf.Abs(transform.position.x - closestPoint.x) > Mathf.Abs(transform.position.y - closestPoint.y))
+                {
+                    transform.position = new Vector3(oldX, transform.position.y, transform.position.z);
+                    if (transform.position.x > obstacle.transform.position.x)
+                    {
+                        Debug.Log("t1");
+                        transform.position = new Vector3(obstacle.transform.position.x + obstacle.GetComponent<BoxCollider2D>().bounds.extents.x + gameObject.GetComponent<BoxCollider2D>().bounds.extents.x + .001f, transform.position.y, transform.position.z);
+                    }
+                    if (transform.position.x < obstacle.transform.position.x)
+                    {
+                        Debug.Log("t2");
+                        transform.position = new Vector3(obstacle.transform.position.x - obstacle.GetComponent<BoxCollider2D>().bounds.extents.x - gameObject.GetComponent<BoxCollider2D>().bounds.extents.x - .001f, transform.position.y, transform.position.z);
+                    }
+                }
+                else
+                {
+                    transform.position = new Vector3(transform.position.x, oldY, transform.position.z);
+                    if (transform.position.y > obstacle.transform.position.y)
+                    {
+                        Debug.Log("t3");
+                        transform.position = new Vector3(transform.position.x, obstacle.transform.position.y + obstacle.GetComponent<BoxCollider2D>().bounds.extents.y + gameObject.GetComponent<BoxCollider2D>().bounds.extents.y + .001f, transform.position.z);
+                    }
+                    if (transform.position.y < obstacle.transform.position.y)
+                    {
+                        Debug.Log("t4");
+                        transform.position = new Vector3(transform.position.x, obstacle.transform.position.y - obstacle.GetComponent<BoxCollider2D>().bounds.extents.y - gameObject.GetComponent<BoxCollider2D>().bounds.extents.y - .001f, transform.position.z);
+                    }
+                }
+            }
+        }
     }
 
 
