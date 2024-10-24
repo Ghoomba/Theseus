4d111cb699b903d1e61c817f92514be2449954f2
sxr7411 <sxr7411@GOL2000-12>
2024-10-24 17:18:18 -0400

    ship moves along path

--- Siren Game/Assets/Scripts/EnemyChase.cs
+++ Siren Game/Assets/Scripts/EnemyChase.cs
@@ -4,47 +4,93 @@ using UnityEngine;
 
 public class EnemyChase : MonoBehaviour
 {
-    float speed;
+    private float speed;
     float rotationSpeed;
     Vector2 originalPosition;
     GameObject player;
 
+    [SerializeField] Transform[] Points;
+
     private float distance;
+    private int pointsIndex;
 
     // Start is called before the first frame update
     void Start()
     {
-        speed = 2.0f;
+        speed = 1.0f;
         rotationSpeed = 60.0f;
-        originalPosition = transform.position;
+        transform.position = Points[0].transform.position;
         player = GameObject.Find("Siren");
+
+        pointsIndex = 0;
+
+        //originalPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        distance = Vector2.Distance(transform.position, player.transform.position);
+        //distance = Vector2.Distance(transform.position, player.transform.position);
+        //
+        //Vector2 direction = player.transform.position - transform.position;
+        //direction.Normalize();
+        //
+        //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        //
+        //angle += -90f;
+        //
+        //
+        //if (distance < 4)
+        //{
+        //    transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        //
+        //    Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        //
+        //    transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        //}
+        //else
+        //{
+        //    transform.position = Vector2.MoveTowards(transform.position, originalPosition, speed * Time.deltaTime);
+        //}
 
-        Vector2 direction = player.transform.position - transform.position;
-        direction.Normalize();
 
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-        angle += -90f;
-        
-
-        if (distance < 4)
+        if (Points.Length == 0)
         {
-            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
 
-            Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, angle));
-
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
         }
         else
         {
-            transform.position = Vector2.MoveTowards(transform.position, originalPosition, speed * Time.deltaTime);
+
+            if (pointsIndex <= Points.Length - 1)
+            {
+
+                distance = Vector2.Distance(transform.position, Points[pointsIndex].transform.position);
+
+                Vector2 direction = Points[pointsIndex].transform.position - transform.position;
+                direction.Normalize();
+
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                angle += -90f;
+
+                transform.position = Vector2.MoveTowards(this.transform.position, Points[pointsIndex].transform.position, speed * Time.deltaTime);
+
+                Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, angle));
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+
+                if (transform.position == Points[pointsIndex].transform.position)
+                {
+
+                    pointsIndex += 1;
+                }
+
+                if (pointsIndex == Points.Length)
+                {
+                    pointsIndex = 0;
+                }
+            }
         }
-        
+
     }
 }
